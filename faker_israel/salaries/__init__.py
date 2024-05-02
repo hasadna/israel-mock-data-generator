e198e533cefb586ce7cefddeed72a0ebff313db7
Ori Hoch <ori@uumpa.com>
2024-05-02 19:58:01 +0300

    add east and west salary slip stubs

--- faker_israel/salaries/__init__.py
+++ faker_israel/salaries/__init__.py
@@ -2,8 +2,12 @@ from functools import partial
 
 from .north import SalaryNorth
 from .south import SalarySouth
+from .east import SalaryEast
+from .west import SalaryWest
 
 SALARIES = {k: partial(v, k) for k, v in {
     'north': SalaryNorth,
     'south': SalarySouth,
+    'east': SalaryEast,
+    'west': SalaryWest,
 }.items()}
---
+++ faker_israel/salaries/east.py
@@ -0,0 +1,32 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+from .salary import Salary
+
+from .. import common_draw, common
+
+
+class SalaryEast(Salary):
+
+    def generate(self):
+        fake = self.fake
+        fixed_context = {}
+        first_year = fake.random_int(2012, 2022)
+        first_month = fake.random_int(1, 10)
+        first_date = datetime.date(first_year, first_month, 1)
+        for salary_date_num, salary_date in enumerate([
+            first_date,
+            first_date + relativedelta(months=1),
+            first_date + relativedelta(months=2),
+        ]):
+            common_draw.save_render_html(
+                output_path=self.item_context.png_output_path.replace('.png', f'-m{salary_date_num+1}.png'),
+                pdf_output_path=self.item_context.pdf_output_path.replace('.pdf', f'-m{salary_date_num+1}.pdf') if self.item_context.pdf_output_path else None,
+                render_path='salaries/east.html',
+                page=self.html_page,
+                http_server_port=self.subtype_context.http_server_port,
+                width=2480, height=3507,
+                context={
+                    **fixed_context,
+                },
+            )
